95be2bf43461136d52bac94428682d9f5fee0dfe
Mateusz <nowe7246@gmail.com>
2022-09-02 14:53:01 +0200

    Created remove product review method and route for it.

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -1,14 +1,12 @@
 import {Product} from "../Models/Product.js";
 import {NotFoundError, ValidationError} from "../utils/error.js";
 import {ReviewSchema} from "../Models/Review.js";
+import {validateLength} from "../utils/validation.js";
 
-export const productReview = async (req, res) => {
-
+// ADD SINGLE PRODUCT REVIEW
+export const addProductReview = async (req, res) => {
     const {rating, comment} = req.body
-
-    if(comment.length >300) {
-        throw new ValidationError('Opis nie może być dłuższy niż 300 znaków')
-    }
+    validateLength(comment, 300, "Opis nie może być dłuższy niż 300 znaków, ani być pusty.")
 
     const product = await Product.findById(req.params.id);
 
@@ -32,9 +30,54 @@ export const productReview = async (req, res) => {
         product.rating = product.reviews.reduce((prev, curr) => curr.rating + prev, 0) / product.reviews.length;
 
         await product.save()
-        res.status(201).json({message:"Recenzja została dodana"});
+        res.status(201).json({message: "Recenzja została dodana"});
 
     } else {
         throw new NotFoundError('Nie znaleziono psroduktu');
     }
+}
+
+//REMOVE PRODUCT REVIEW
+export const removeReview = async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    const {rating, productId, review_id} = req.body;
+
+
+    if (!product) {
+        throw new NotFoundError('Produkt nie znaleziono');
+    }
+    if (!rating || rating <= 0 || rating > 5) {
+        throw new ValidationError('Ocena nie moze być puta ani mniejsza od 0 i większa od 5');
+    }
+    if (!review_id || typeof review_id !== "string") {
+        throw new ValidationError('Id recenzji nie może być puste ani i musi być typu string');
+    }
+    if (!productId || typeof productId !== "string") {
+        throw new ValidationError('Id produktu nie może być puste ani i musi być typu string');
+    }
+
+    try {
+        let newRating;
+        let newNumReviews;
+        if (product.reviews.length === 1) {
+            newRating = 0;
+            newNumReviews = 0
+        } else {
+            newRating = (product.reviews.reduce((prev, curr) => (curr.rating) + prev, 0) - rating) / (product.reviews.length - 1)
+            newNumReviews = --product.numReviews;
+        }
+        const newReviews = product.reviews.filter((review) => review.user.toString() !== productId)
+
+        await Product.findOneAndUpdate({_id: req.params.id}, {
+            reviews: newReviews,
+            numReviews: newNumReviews,
+            rating: newRating
+        })
+
+        await ReviewSchema.findByIdAndDelete(review_id);
+        res.status(200).json({message: "Usunięto pomyślnie recenzje."});
+    } catch (e) {
+        throw new ValidationError(e.message)
+    }
+
 }
--- routes/review.route.js
+++ routes/review.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { productReview} from "../controllers/review.controller.js";
+import {addProductReview, removeReview} from "../controllers/review.controller.js";
 import {protect} from "../middleware/auth.middleware.js";
 
 export const reviewRoute = express.Router()
 
 
-reviewRoute.post('/:id', protect, productReview)
+reviewRoute
+    .post('/:id', protect, addProductReview)
+    .put('/:id',protect,removeReview)
